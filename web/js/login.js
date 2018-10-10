c77e6b471df2f4038f8fd0c71440160dc61d0fa8
Programmeryong <34471838+Programmeryong@users.noreply.github.com>
2018-10-10 14:42:24 +0800

    crm1.2
    注册页面

--- web/js/login.js
+++ web/js/login.js
@@ -1,6 +1,31 @@
 $(function(){
-	// var i = $(document).width();
-	// var j = $(document).height();
-	// $('body').css({'width':i,'height':j});
-	// console.log( i +"+"+ j);
+	$('.login_content li').hide();
+	$('.login_nav li').on('click',function(){
+		var i = $(this).index();
+		$('.login_nav li').removeClass('thisnavli');
+		$(this).addClass('thisnavli');
+		$('.login_content li').hide();
+		$('.login_content li').eq(i).show();
+	})
+	$('.login_nav li').eq(0).click();
+	var ii =0;
+	$('.thischeckbox').click(function(){
+		ii++;
+		if(ii%2 ==0){
+			// 不变色
+			$('.thischeckbox').removeClass('icon-duoxuankuang1');
+			$('.thischeckbox').addClass('icon-icon-');
+			$('.thischeckbox').css({'color':'rgba(0,0,0,.45)'});
+			$('.thisbox').click();
+		}else{
+			// 变蓝色
+			$('.thischeckbox').removeClass('icon-icon-');
+			$('.thischeckbox').addClass('icon-duoxuankuang1');
+			$('.thischeckbox').css({'color':'#1A8CFF'});
+			$('.thisbox').click();
+		}
+	})
+	$('.clickthisbtn').click(function(){
+		$('.thanbtn').click();
+	})
 })
