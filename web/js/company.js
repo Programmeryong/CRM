772bd5c1bbee570a1d82d75d9ad9b36f939c9bdc
Programmeryong <34471838+Programmeryong@users.noreply.github.com>
2018-10-11 16:38:32 +0800

    a96e54rf

---
+++ web/js/company.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('.no_this_btn').click(function(){
+		$('.no_this_btn').animate({left: '+150px',opacity: '0'}, 500);
+		$('.yes_this_btn').animate({left: '+100px',opacity: '1'}, 500);
+	})	
+	$('.clickthisbtn').click(function(){
+		$('.yes_this_btn').animate({left: '+50px',opacity: '0'}, 500)
+		$('.no_this_btn').animate({left: '+100px',opacity: '1'}, 500);
+	})
+})
