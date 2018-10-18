ca99fd069946c91071b2c0c92f9aeba88ff3b183
wenhuayan <40262642+wenhuayan@users.noreply.github.com>
2018-10-18 09:22:47 +0800

    fd
    sdf

--- web/js/crm-style.js
+++ web/js/crm-style.js
@@ -4,5 +4,17 @@ $(function(){
 		$(this).addClass('active').siblings().removeClass('active');
 	})
 	
+	// 点击回到顶部
+	$('.gototop').click(function () {
+        $('body,html').animate({scrollTop: 0}, 1000);
+    })
 	
+	$('.clickSM').hover(function(){
+		$('.thisSM').stop();
+		$('.thisSM').fadeIn(500);
+	},function(){
+		$('.thisSM').stop();
+		$('.thisSM').fadeOut(500);
+	})
+    
 });
