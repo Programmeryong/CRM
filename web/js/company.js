88209422edd1c15453c93ff87d2bd1bf7c409783
wenhuayan <40262642+wenhuayan@users.noreply.github.com>
2018-12-07 14:08:09 +0800

    6565+561
    32052

--- web/js/company.js
+++ web/js/company.js
@@ -1,12 +1,12 @@
 $(function(){
-	$('.no_this_btn').click(function(){
-		$('.no_this_btn').animate({left: '+150px',opacity: '0'}, 500);
-		$('.yes_this_btn').animate({left: '+100px',opacity: '1'}, 500);
-	})	
-	$('.clickthisbtn').click(function(){
-		$('.yes_this_btn').animate({left: '+50px',opacity: '0'}, 500)
-		$('.no_this_btn').animate({left: '+100px',opacity: '1'}, 500);
-	})
+//	$('.no_this_btn').click(function(){
+//		$('.no_this_btn').animate({left: '+150px',opacity: '0'}, 500);
+//		$('.yes_this_btn').animate({left: '+100px',opacity: '1'}, 500);
+//	})	
+//	$('.clickthisbtn').click(function(){
+//		$('.yes_this_btn').animate({left: '+50px',opacity: '0'}, 500)
+//		$('.no_this_btn').animate({left: '+100px',opacity: '1'}, 500);
+//	})
 	
 	
 	/*控制提供/需求的列表的标题*/
